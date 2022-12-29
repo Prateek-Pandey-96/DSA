58d77cc46c66441a68106629413478ffe730f408
prateek9928 <prateek@theox.co>
2022-12-29 23:12:13 +0530

    stack problems

---
+++ StackProblems/OnlineStockSpan(LC-901).cpp
@@ -0,0 +1,24 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>> s;
+    StockSpanner() {
+        while(!s.empty())
+            s.pop();
+    }
+    
+    int next(int price) {
+        int days = 1;
+        while(!s.empty() and s.top().first <= price){
+            days += s.top().second;
+            s.pop();
+        }
+        s.push({price, days});
+        return days;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
